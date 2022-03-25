226497e0d75ac74fd9d190d3864421c9076bbb84
Luiz Carlos <luizgadao@gmail.com>
2022-03-25 15:39:03 -0300

    hide toolbar

--- app/src/main/java/br/com/luizgadao/androiddiotqiinstagramstories/MainActivity.kt
+++ app/src/main/java/br/com/luizgadao/androiddiotqiinstagramstories/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
         viewPager.adapter = AdapterStories()
         viewPager.setPageTransformer(CubeTransformer())
